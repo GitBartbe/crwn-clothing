a6ed66e991d3b0e16d452048948a05105b031664
GitBartbe <Bartosz.bednarski@wp.pl>
2021-11-17 22:06:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+import Hompage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div ><Hompage /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/directory-component/directory.component";
+import './homepage.styles.scss'
+
+
+
+
+const Hompage = () => (
+    <div className='homepage'>
+   <Directory/>
+    </div>
+)
+
+export default Hompage;
