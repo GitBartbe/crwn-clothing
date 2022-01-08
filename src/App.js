27a3ed28b50a6e81cf2af4d42af0648dd1f1b4a6
GitBartbe <Bartosz.bednarski@wp.pl>
2022-01-08 20:57:48 +0100

    added redux and user reducer and user actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
+import { connect } from "react-redux";
+
 import ShopComponent from "./pages/shop/shop.component";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import Header from "./components/header/header.component";
 import Sign from "./pages/sign-in-up/sign-in-up";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./user/user.actions";
 
 const HatsPage = () => (
   <div>
@@ -14,37 +17,27 @@ const HatsPage = () => (
 );
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
 
   unsubscribeFromAuth = null;
 
   //onAuthStateChanged is a method on firebase lirbary and geting user from object returned from google auth
+  
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      
+
+   const {setCurrentUser} = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          }
-          );
-          
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
-       
       }
-      this.setState({currentUser: userAuth}) // if user is unsubscribed than user object is empty. State is cleared
+      setCurrentUser(userAuth); // if user is unsubscribed than user object is empty. State is cleared
     });
   }
 
@@ -55,7 +48,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopComponent />} />
@@ -65,5 +58,8 @@ class App extends React.Component {
     );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/user/user.actions.js
@@ -0,0 +1,4 @@
+export const setCurrentUser = user => ({
+    type: 'SET_ CURRENT_USER',
+    payload: user
+});
