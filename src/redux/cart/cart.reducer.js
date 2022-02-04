2d7291e9b2b5b441bd83915c333f43e061bd5f52
GitBartbe <bartosz.bednrski@wp.pl>
2022-02-04 12:02:38 +0100

    added clear item from card

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,29 +1,41 @@
-import CartActionsTypes from './cart.types'
+import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
 
-const  INITIAL_STATE = {
-    hidden : true,
-    cartItems: []
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: []
 };
 
-const cartReducer = (state = INITIAL_STATE, action ) => {
-    switch (action.type) {
-        case CartActionsTypes.TOGGLE_CART_HIDDEN: 
-        return {
-            ...state,
-            hidden: !state.hidden
-        }
-        case CartActionsTypes.ADD_ITEM:
-            return {
-                ...state,
-                cartItems:addItemToCart(state.cartItems, action.payload)
-            }
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
 
-        default: return state;    
-    };
+      case CartActionTypes.REMOVE_ITEM:
+      return  
+      {
+           
+        };
+        
 
-    
-    
-}
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
+    default:
+      return state;
+  }
+};
 
 export default cartReducer;
